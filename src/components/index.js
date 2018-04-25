6c4310b6a52037bc5c16cd7498f4c87776b39a7a
lsy <272631750@qq.com>
2018-04-25 20:26:33 +0800

    更新

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import vuePayPop from './vue-pay-pop.vue' // 导入组件
 const PayPop = {
     install(Vue, options) {
-        Vue.component(vuePayPop.name, vuePayPop) // vuePayKeyboard.name 组件的name属性
+        Vue.component(vuePayPop.name, vuePayPop) // vuePayPop.name 组件的name属性
         // 类似通过 this.$xxx 方式调用插件的 其实只是挂载到原型上而已
         // Vue.prototype.$xxx  // 最终可以在任何地方通过 this.$xxx 调用
         // 虽然没有明确规定用$开头  但是大家都默认遵守这个规定
