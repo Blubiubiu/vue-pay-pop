5e64fed305336ec1cad78356da4daa5bb16421ca
lsy <272631750@qq.com>
2018-04-26 11:30:36 +0800

    更新

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,6 +5,11 @@ const PayPop = {
         // 类似通过 this.$xxx 方式调用插件的 其实只是挂载到原型上而已
         // Vue.prototype.$xxx  // 最终可以在任何地方通过 this.$xxx 调用
         // 虽然没有明确规定用$开头  但是大家都默认遵守这个规定
+    },
+    render(h) {
+      return h('div', {
+        class: 'vuepaypop'
+      }, [])
     }
 }
 if (typeof window !== 'undefined' && window.Vue) {
